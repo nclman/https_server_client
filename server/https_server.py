c199ce1da22975b804f15024760f4feadb669c38
nclman <nclman@users.noreply.github.com>
2024-06-19 13:53:38 +0700

    Send binary data from server

--- server/https_server.py
+++ server/https_server.py
@@ -10,28 +10,42 @@ logging.basicConfig(filename='https_server.log', level=logging.INFO)
 
 from io import BytesIO
 
+getData = "GET response body!"
+postData = [0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38]
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        logger.info("Received HEAD")
+        self.send_response(200)
+        self.send_header("Content-Type", "application/octet-stream") # this is sent
+        self.end_headers()
+        self.wfile.write(b'HEAD response!') # does not get sent
+        print(self.path)
+
     def do_GET(self):
         logger.info("Received GET")
         self.send_response(200)
+        self.send_header("Content-Type", "application/octet-stream")
+        self.send_header("Content-Length", str(len(getData.encode('utf-8'))))
         self.end_headers()
-        self.wfile.write(b'Hello, World!')
+        self.wfile.write(bytearray(getData.encode('utf-8')))
+        print(self.path)
 
     def do_POST(self):
         logger.info("Received POST")
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
+        self.send_header("Content-Type", "application/octet-stream")  # for binary data
+        self.send_header("Content-Length", str(len(postData)))
         self.end_headers()
 
         # Print body content of POST
         print(body)
 
-        # Send response to client
+        # Send binary response to client
         response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
+        response.write(bytearray(postData))
         self.wfile.write(response.getvalue())
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
